3cdb8b4c101a9374320cd26711736a3dc1979cdf
lorenzoromani1983 <24575445+lorenzoromani1983@users.noreply.github.com>
2023-12-17 19:34:19 +0100

    Add files via upload
    Co-mentioned is a very simple yet effective tool that helps finding co-mentions of keywords in web pages.
    
    While doing research on entities of interest (companies, people, whatever) you may need to check if two or more of the target entities are mentioned by the same web page at the same time. 
    
    This tool comes in handy if you have multiple keywords/entities and need to quickly assess whether there are interesting co-mentions.
    
    You will first need to install some python modules:
    
    > pip install inscriptis
    > pip install numpy
    > pip install unidecode
    
    Now you will need a ScaleSerp API: co-mentioned searches Google via a third-party API. Free tokens are available at ScaleSerp and paid ones are fairly cheap too. 
    
    Save your token in the 'token.txt' file.
    
    Now, type your target keywords/entities in the 'entities.txt' file. One line, one entity. No quotes:
    
    alaeddin senguler
    trend gyo
    hamas
    
    Now you're set and can run the co-mentioned.py file:
    
    > python3 co-mentioned.py
    
    The tool will first combine the entities you saved in the entities.txt file generating as many keyword pairs as possible. Then it will search each pair on Google in order to retrieve urls. In our case, it will look up on Google:
    
    "alaeddin senguler" "trend gyo"
    "alaeddin senguler" "hamas"
    "hamas" "trend gyo"
    
    Quotes are added automatically, so you won't need to enclose keywords in quotes in the entities.txt file. 
    
    You can use non standard characters (è,é, à,ò), etc. It may increase the quality of Google search results but it won't affect the way co-mentioned works, as all text is unidecoded for consistency.
    
    In the previous case, just three keyword pairs can be created. Of course, the more entities you type in entities.txt, the more pairs you will get (this may quickly exhaust your ScaleSerp account).
    
    If your ScaleSerp token has enough API calls to complete the search job, it will search Google for URLs. 
    Next, it will start opening each url in order to:
    
    a) detect if at least 2 keywords are on the web page
    b) compute the minimum distance between the keywords
    
    If more than 1 keyword are on the same page, co-mentioned computes the minimum distance between the keywords and saves the results in the REPORT.csv file.
    
    Note that co-mentioned *searches* keyword pairs but *detects* all the keywords that you typed in the entities.txt file.
    
    The REPORT.csv file is created automatically in the local directory and contains three columns separated by '|':
    
    URL|Entities|Distance
    
    Last but not least, in the case where three or more entities are matched in the same web page, the "Distance" indicator is about the closest pair.
    
    Have fun

---
+++ co-mentioned.py
@@ -0,0 +1,150 @@
+import requests
+import unidecode
+import itertools
+import re
+import os
+import csv
+import json
+from inscriptis import get_text
+import numpy as np
+from multiprocessing import Pool, Manager
+from functools import partial
+import time
+
+def search(combination, urls, token):
+    print("Searching: ", combination)
+    response = requests.get(
+        "https://api.scaleserp.com/search",
+        params={"api_key": token, "q": '"' + combination[0] + '"' + " " + '"' + combination[1] + '"', "num":100},
+    ).json()
+    if "organic_results" in response:
+        for row in response["organic_results"]:
+            if "link" in row and "snippet" in row:
+                link = row["link"]
+                urls.append(link)
+    else:
+        print("[!] No results available or query limit reached")
+
+def check_remaining_requests(token):
+    response = requests.get("https://api.scaleserp.com/account", params={"api_key": token}).json()
+    remaining_requests = response["account_info"]["credits_remaining"]
+    return remaining_requests
+
+def check_url(url, pattern, done):
+    attempts = 0
+    while attempts < 4:
+        try:
+            corpus = get_text(requests.get(url, timeout=20).text)
+        except Exception as e:
+            print(f"Error on url {url}, :{e}")
+            attempts += 1
+            continue
+        body = unidecode.unidecode(corpus).lower()
+        results = pattern.findall(body)
+        indexes_list = list()
+        if len(set(results)) > 1:
+            tokens = list()
+            for entity in set(results):
+                token = "_".join(entity.split())
+                tokens.append(token)
+                new_body = re.sub(entity, token, body)
+                body = new_body
+            without_punctuation = re.sub("[^0-9a-zA-Z&@_+]+", " ", body)
+            single_spaced_text = re.sub("\s+", " ", without_punctuation).strip()
+            text_to_list = single_spaced_text.split()
+            for token in tokens:
+                entity_indexes = [
+                    index for index, value in enumerate(text_to_list) if value == token
+                ]
+                indexes_list.append(entity_indexes)
+            all_diffs = list()
+            products = list(itertools.product(*indexes_list))
+            for distance_eval in products:
+                combinations = itertools.combinations(distance_eval, 2)
+                for combination in combinations:
+                    diff = min(abs(np.diff(combination)), default=0)
+                    all_diffs.append(diff)
+            minimum_diff = min(all_diffs, default=0)
+            evaluated_source = {'url': url, 'entities': set(results), 'distance': minimum_diff}
+            done.append(evaluated_source)
+            print(minimum_diff, " >>> ", set(results), " >>> ", url)
+            break
+
+        if len(set(results)) <= 1:
+            break
+        
+        if attempts == 4:
+            break
+
+def main():
+    
+    manager = Manager()
+    evaluated = manager.list()
+    path = os.path.dirname(os.path.abspath(__file__))
+    file = os.path.join(path, "REPORT"+".csv")        
+    
+    targets = [
+        unidecode.unidecode(target.rstrip("\n").lower())
+        for target in open(os.path.join(path, "entities.txt")).readlines()
+    ]
+
+    token_ = [
+        key.rstrip("\n")
+        for key in open(os.path.join(path, "token.txt")).readlines()
+    ]
+    
+    token = token_[0]
+    
+    credits_available = check_remaining_requests(token)
+    
+    pattern = re.compile("|".join(targets))
+    combos = list(itertools.combinations(targets, 2))
+    
+    if len(combos) <= credits_available:
+        
+        print(f"{credits_available} ScaleSerp requests left")
+    
+        print("Iterating over " + str(len(combos)) + " combinations\n")
+    
+        print("Search started\n")
+    
+        manager = Manager()
+        links = manager.list()
+        kwargs = {"urls": links, 'token': token}
+        p = Pool(os.cpu_count())
+        mapfunc = partial(search, **kwargs)
+        p.map(mapfunc, combos)
+        p.close()
+        p.join()
+        
+        print("\nSearch finished\n")
+        
+        print("Investigating "+ str(len(set(links)))+" sources\n")
+    
+        manager = Manager()
+        kwargs = {"pattern": pattern, "done": evaluated}
+        p = Pool(os.cpu_count())    
+        mapfunc = partial(check_url, **kwargs)
+        p.map(mapfunc, set(links))
+        p.close()
+        p.join()
+        
+        with open(file,'w', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter ="|" )
+            header = ['URL','Entities','Distance']
+            writer.writerow(header)
+            for source in evaluated:
+                rows = [source['url'], source['entities'], source['distance']]
+                writer.writerow(rows)
+                
+        print("\nResults saved in "+file)
+    
+    else:
+        print("Credits limit reached. Replace your ScaleSerp token")
+    
+if __name__ == "__main__":
+    main()
+    
+
+    
+  
